f778325c471a0d81abaa7aead9aa253f45c91a08
AmanGupta262 <aman@aman>
2021-03-07 12:47:12 +0530

    complete

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # dog-gallery
-A website to fetch dog images of breeds and sub-breeds using Dog API ( https://dog.ceo/dog-api/documentation/)
+##### A website to fetch dog images of breeds and sub-breeds using Dog API 
+`( https://dog.ceo/dog-api/documentation/)`
+
+##### Home page look like this 
+
+![](home.png)
+
+##### When a breed has no sub-breed, then the web page looks like this -
+
+![](breed.png)
+
+##### When a breed has sub-breed, then the web page looks like this -
+
+![](sub-breed.png)
+
---
+++ script.js
@@ -0,0 +1,40 @@
+/// <reference path="../jquery-3.5.1.js" />
+var breeds;
+$.get('https://dog.ceo/api/breeds/list/all', function (data) {
+    breeds = data.message;
+});
+
+$('document').ready(function () {
+    var keys = Object.keys(breeds);
+    keys.forEach(e => {
+        $('#breeds').append(`<option value="${e}">${e}</option>`)
+    });
+});
+
+$('#breeds').on('change', function () {
+    var selectedBreed = $('#breeds').val();
+    var subBreeds = breeds[selectedBreed]
+    $('#sub-breeds').css('display', 'none');
+    $('#sub-breeds').html("<option value=''>Select Sub Breed</option>");
+    if(subBreeds.length !== 0){
+        subBreeds.forEach(e => {
+            $('#sub-breeds').append(`<option value="${e}">${e}</option>`);
+        });
+        $('#sub-breeds').css('display', 'block');
+    }
+});
+
+$('#get').on('click', function () {
+    var breedName = '';
+    $('.result').html("");
+    if ($('#sub-breeds').val() !== '')
+        breedName = $('#breeds').val() + '/' + $('#sub-breeds').val();
+    else
+        breedName = $('#breeds').val();
+    $.get(`https://dog.ceo/api/breed/${breedName}/images`, function (data) {
+        let images = data.message;
+        images.forEach(e => {
+            $('.result').append(`<img src="${e}" alt="">`)
+        });
+    });
+});
